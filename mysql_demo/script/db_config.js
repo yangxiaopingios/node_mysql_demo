4a16b6f4781a4896338265f1a9b4317ee60f3890
yangxiaoping <yangxiaoping@ihealthlabs.com.cn>
2017-04-26 10:59:39 +0800

    修改配置文件

--- mysql_demo/script/db_config.js
+++ mysql_demo/script/db_config.js
@@ -2,10 +2,10 @@
  * Created by yangyang on 17/4/25.
  */
 module.exports  = {
-    host: '172.16.4.196',
-    user: 'root',
-    password: '111111',
-    database: 'wx_timingSendTemplateMessage',
-    port: 3306,
-    dateStrings:true
+    host: 'host',
+    user: 'mysql用户名',
+    password: 'mysql密码',
+    database: '连接的数据库名字',
+    port:port,
+    dateStrings:true  
 }
